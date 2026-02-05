734f1c0658087ee3d1d476925a60ff86608df9f2
Nick Wydra <nick@nautiluslabs.co>
2026-02-05 10:07:56 -0500

    feat(splash): Use wyd component

--- src/components/wyd.tsx
+++ src/components/wyd.tsx
@@ -1,23 +1,27 @@
 import { h } from "preact";
 
+import "./wyd.css";
+
+h;
+
 export const wyd = () => (
   <div class="canvas">
     <div class="bracket b-l"></div>
 
-    <div class="letter w">
-      <div class="bar left"></div>
-      <div class="bar mid" style="border-radius: 40px 40px 0 0"></div>
-      <div class="bar right" style="border-bottom-right-radius: 40px"></div>
-    </div>
-
-    <div class="letter y">
-      <div class="bar left" style="border-radius: 0 40px"></div>
-      <div class="bar right"></div>
-    </div>
-
-    <div class="letter d">
-      <div class="bar left" style="border-radius: 40px 0 0 40px"></div>
-      <div class="bar right" style="border-radius: 40px 0"></div>
+    <div className="letters">
+      <div class="letter w">
+        <div class="bar left"></div>
+        <div class="bar mid"></div>
+        <div class="bar right"></div>
+      </div>
+      <div class="letter y">
+        <div class="bar left"></div>
+        <div class="bar right"></div>
+      </div>
+      <div class="letter d">
+        <div class="bar left"></div>
+        <div class="bar right"></div>
+      </div>
     </div>
 
     <div class="bracket b-r"></div>
--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,13 @@
 import { h, render } from "preact";
+import { wyd as Wyd } from "./components/wyd";
 import "./style.css";
+
+h;
 
 function App() {
   return (
     <div id="title">
-      <svg id="title-svg" viewBox="0 0 500 100" preserveAspectRatio="none">
-        <text
-          x="0"
-          y="60"
-          textLength="500"
-          textAnchor="middle"
-          lengthAdjust="spacingAndGlyphs"
-          fontFamily="Courier, monospace"
-          letter-spacing="0px"
-          style="font-family: sans-serif; font-size: 40px; font-weight: bold;"
-          fill="currentColor"
-        >
-          [wyd]
-        </text>
-      </svg>
+      <Wyd />
     </div>
   );
 }
